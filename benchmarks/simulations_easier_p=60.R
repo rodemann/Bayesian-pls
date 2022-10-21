0f595a8b7a0ec93fed42bc264bb7e974c1f8cd39
Julian Rodemann <julian.rodemann@freenet.de>
2022-10-21 11:12:18 +0200

    ...

---
+++ benchmarks/simulations_easier_p=60.R
@@ -0,0 +1,22 @@
+library(dplyr)
+
+set.seed(2037420)
+
+N = 1
+share_setups = c(0.8)
+n_setups = c(800,1000,1200,1400)
+
+for (share_unlabeled in share_setups) {
+  for (n in n_setups){
+    share_unlabeled %>% print
+    n %>% print()
+    try(
+      source(paste(getwd(),"/benchmarks/run_benchmarks_simulated_data_easier_p=60.R", sep=""))
+    )
+    try(
+      source(paste(getwd(),"/analyze/analyze.R", sep=""))
+    )
+    print(Sys.time()) 
+  }
+}
+
